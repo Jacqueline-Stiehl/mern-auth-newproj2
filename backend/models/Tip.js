8fa052d96966c2c4326ba7ffaf64257c5e52698c
J-Stiehl <salsajackie@yahoo.com>
2023-10-10 14:11:59 -0500

    initial set up and Tip.js file

---
+++ backend/models/Tip.js
@@ -0,0 +1,45 @@
+const { Schema, model, Types, ObjectId } = require("mongoose");
+
+const tipSchema = new Schema(
+  {
+    topic: {
+      type: String,
+      required: true,
+    },
+
+    upvote: {
+      type: integer,
+    },
+
+    downvote: {
+      type: integer,
+    },
+
+    isActive: {
+      type: boolean,
+      default: true,
+      required: true,
+    },
+
+    userId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // dateCreated: {
+    //   type: Date,
+    //   default: Date.now
+    // },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Journal = model("Tip", tipSchema);
+
+module.exports = Tip;
